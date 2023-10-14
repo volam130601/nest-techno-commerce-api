c0e80bcfb93b8cb0c128f0eb8d951f2c75849a8c
volam130601 <volam13062001@gmail.com>
2023-10-14 10:23:22 +0700

    install depencencies

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {
+
+    }
+    @Post('register')
+    register() {
+        return 'Register...'
+    }
+
+    @Post('login')
+    login() {
+        return 'Login...'
+    }
+}
