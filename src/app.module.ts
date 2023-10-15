5e70d197812c5810912028b3022c47fb85550f20
volam130601 <volam13062001@gmail.com>
2023-10-15 09:36:20 +0700

    dev/config-prisma-service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-
+import {
+  ConfigModule
+} from '@nestjs/config'
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule]
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), UserModule, AuthModule, PrismaModule]
 })
 export class AppModule { }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -14,4 +14,21 @@ export class PrismaService extends PrismaClient {
         })
     }
 
+
+    cleanDatabase() {
+        //In a 1 - N relation, delete N firstly, then delete '1'
+        return this.$transaction([
+            this.orderItem.deleteMany(),
+            this.payment.deleteMany(),
+            this.shipment.deleteMany(),
+            this.category.deleteMany(),
+            this.cart.deleteMany(),
+            this.wishlist.deleteMany(),
+            this.discount.deleteMany(),
+            this.product.deleteMany(),
+            this.order.deleteMany(),
+            this.customer.deleteMany(),
+        ])
+    }
+
 }
